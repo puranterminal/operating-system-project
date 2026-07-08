644894a80756ad54b5f082870e21ce216d2187c8
Puran Rijal <puranrijal246@gmail.com>
2026-07-08 20:58:34 +0545

    feat(backend): add skeleton with main function and constants

---
+++ Task1/backend.c
@@ -0,0 +1,27 @@
+﻿#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/stat.h>
+#include <pwd.h>
+
+#define SOCKET_PATH    "/tmp/priv_auth.sock"
+#define MAX_USERNAME   64
+#define MAX_PASSWORD   128
+#define BACKLOG        8
+#define AUTH_MAGIC     0xCAFEBABE
+#define NOBODY_UID     65534
+
+int main(void) {
+    printf("\n  [BACKEND] Process started  PID=%d  EUID=%d\n\n",
+           getpid(), geteuid());
+    printf("  [BACKEND] Waiting for connections...\n");
+    return EXIT_SUCCESS;
+}
